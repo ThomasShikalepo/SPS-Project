a48b6415fdf3dc3ec08d37db5964ad8224a8b575
Marcy-mm <marcellinemuhapili@gmail.com>
2025-04-20 10:07:33 +0200

    Additional features to dashboard

--- client/src/hooks/useCarousel.ts
+++ client/src/hooks/useCarousel.ts
@@ -1,12 +1,14 @@
 import { useState, useEffect } from "react";
-
+//interface for props that we need to pass
 interface UseCarouselProps {
   totalImages: number;
   interval?: number;
 }
 
+//grab an image to cycle through each time period
 export const useCarousel = ({
   totalImages,
+  //5 seconds
   interval = 5000,
 }: UseCarouselProps) => {
   const [currentImage, setCurrentImage] = useState(0);
