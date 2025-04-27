2bbea003ca85fa5c74dc5e62547d852a80a00d9b
Marcy-mm <marcellinemuhapili@gmail.com>
2025-04-27 23:58:19 +0200

    Addittion to Landing page

--- server/dist/routes/courseRoutes.js
+++ server/dist/routes/courseRoutes.js
@@ -4,8 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const multer_1 = __importDefault(require("multer"));
 const courseControllers_1 = require("../controllers/courseControllers");
+const express_2 = require("@clerk/express");
 const router = express_1.default.Router();
+const upload = (0, multer_1.default)({ storage: multer_1.default.memoryStorage() });
 router.get("/", courseControllers_1.listCourses);
+router.post("/", (0, express_2.requireAuth)(), courseControllers_1.createCourse);
 router.get("/:courseId", courseControllers_1.getCourse);
+router.put("/:courseId", (0, express_2.requireAuth)(), upload.single("image"), courseControllers_1.updateCourse);
+router.delete("/:courseId", (0, express_2.requireAuth)(), courseControllers_1.deleteCourse);
 exports.default = router;
