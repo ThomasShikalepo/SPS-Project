fd3e83ea2d850883bc38ad70daa2cabe239a2bd6
avril <mutatabikwaavril@gmail.com>
2025-04-28 05:07:28 +0200

    Added ClerkClient

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,10 +4,12 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
-import * as dynamoose from "dynamoose"; 
-
+import * as dynamoose from "dynamoose";
+import { createClerkClient } from "@clerk/express"; 
+import userClerkRoutes from "./routes/userClerkRoutes";
 /*ROUTE IMPORTS */ 
 import courseRoutes from "./routes/courseRoutes"
+
 
 /*CONFIGARATION*/
 dotenv.config();
@@ -16,6 +18,10 @@ const isProduction = process.env.NODE_ENV === "production";
 if (!isProduction) {
   dynamoose.aws.ddb.local();
 }
+
+export const clerkClient = createClerkClient({
+  secretKey: process.env.CLERK_SECRET_KEY,
+})
 
 
 const app = express();
@@ -33,6 +39,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/courses", courseRoutes);
+app.use("/users/clerk", userClerkRoutes);
 
 /* SERVER*/ 
 const port = process.env.PORT || 3000;
